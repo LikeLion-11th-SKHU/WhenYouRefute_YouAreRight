606113b0fa9ac82ad3a12e7107a216ebc43ac11b
treaty321 <2117ab@gmail.com>
2023-08-12 21:54:58 +0900

    login view작성

--- account/views.py
+++ account/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import auth
+
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            return redirect("youareright: main")
+        else:
+            return redirect(request, "login")
+    else:
+        form = AuthenticationForm()
+        return render(request, "login.html", {"form": form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("youareright: main")
+
+
+# 회원가입 함수
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(request, user)
+            return redirect("youareright: main")
+        else:
+            return redirect(request, "signup")
+    else:
+        form = UserCreationForm()
+        return render(request, "signup.html", {"form": form})
