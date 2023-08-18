0e64ed5f278335ff96500965129d654a764ade90
강민정 <staccato189@gmail.com>
2023-08-18 20:44:35 +0900

    f

--- youareright/forms.py
+++ youareright/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["category", "title", "user_name", "body", "image"]
+        fields = ["category", "title", "body", "image"]
